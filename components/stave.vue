7b09dcb49c21f9e2a4af708d17988132d3b259f0
Kavelin <34014055+Kavelin@users.noreply.github.com>
2023-11-22 17:00:06 +0000

    stave + vexflow?

---
+++ components/stave.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="stave" ref="el"></div>
+</template>
+<script setup lang="ts">
+import Vex from 'vexflow';
+const el = ref(null);
+onMounted(() => {
+    const { Renderer, Stave } = Vex.Flow;
+    const renderer = new Renderer(el.value!, Renderer.Backends.SVG);
+    renderer.resize(500, 120);
+    const context = renderer.getContext();
+    context.setFont('Arial', 10);
+    const stave = new Stave(0, 0, 400);
+    stave.addClef('treble').addTimeSignature('4/4');
+    stave.setContext(context).draw();
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,16 +10,26 @@
                     
                 </div>
             </div>
-            <div id="editor">
-                <div class="sheet">
+            <div id="editor" >
+                <div class="sheet" :style="{gridAutoRows:'24px ' + parts.map(x => x.staves * 150 + 'px').join(' ')}">
                     <div class="measures">
-                        Measures
-                        <div class="measure-c" :key="i" v-for="(measure, i) in measures">
+                        <div class="bef-space">
+                            Measures
+                        </div>
+                        <div class="measure-c" :style="{width:('domLength' in measure)?(measure.domLength! + 'px') : '150px'}" :key="i" v-for="(measure, i) in measures">
                             {{ i }}
                         </div>
                     </div>
-                    <div class="part" v-for="part in parts">
+                    <div class="part" v-for="part in parts" :style="{}">
                         
+                        <div class="part-name">
+                            {{ part.name }}
+                        </div>
+                        <div class="staves">
+                            <stave v-for="index in part.staves">
+                                hey
+                            </stave>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -33,7 +43,21 @@
     </div>
 </template>
 <script setup lang="ts">
+import stave  from '/components/stave.vue';
 
-let measures = ref(Array(10));
-let parts = ref(Array(3));
+interface Measure {
+    domLength:number;
+    
+}
+
+interface Part {
+    name: string;
+    abrev: string;
+    group?: string;
+    staves: number;
+}
+
+let measures = ref(<Measure[]>[{domLength:200}, {}, {}]);
+let parts = ref(<Part[]>[/*{name:"piano", abrev:"pno.", staves:2}, {name:"bass", abrev:"bs", staves:1}, */{name:"drumset", abrev:"ds", staves:1}]);
+
 </script>
